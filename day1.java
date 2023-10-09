f49ca1851f8f76390c484849d9012c88c8112287
milawatiD0223342 <147219691+milawatiD0223342@users.noreply.github.com>
2023-10-09 19:26:27 +0700

    Add files via upload

---
+++ day1.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        String nama = "Milawati";
+        String alamat = "Tinambung";
+        int umur = 20;
+        double tinggi = 17.5f;
+        boolean status = false;
+        System.out.println("Milawati");
+        System.out.println("Tinambung");
+        System.out.println(umur);
+        System.out.println(tinggi);
+        System.out.println(status);
+        
+        
+    }
+
+}
