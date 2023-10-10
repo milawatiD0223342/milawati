fb0bc612ae6f4ba79df538c3f9239d8e7ffdace7
milawatiD0223342 <147219691+milawatiD0223342@users.noreply.github.com>
2023-10-10 19:28:58 +0700

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int angka = 6;
+        int angka1 = 2;
+        System.out.println(angka+angka+angka1);
+    }
+
+}
