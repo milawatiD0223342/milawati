46d4a51df15bfe4175e145ac977aa8ae76cb052d
milawatiD0223342 <147219691+milawatiD0223342@users.noreply.github.com>
2023-10-11 11:58:44 +0700

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int p = 8;
+          int p1 = 3;
+          int p2 = p+p1;
+        System.out.println(p2);
+        
+    }
+
+}
