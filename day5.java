1b45f7e3e9a421a387e9c8636ec34f237d92acf1
milawatiD0223342 <147219691+milawatiD0223342@users.noreply.github.com>
2023-10-13 18:44:44 +0700

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,18 @@
+package ngoding;
+public class Main {
+	public static void main(String[] args) {
+		int angka = 30;
+		byte nilai = 20;
+		System.out.println("<integer> <byte>");
+		System.out.println("nilai integer angka = " + angka); // cara menentukan jumlah nilai int
+		System.out.println("nilai byte = " + nilai); // cara menentukan jumlah nilai byte
+		System.out.println("Nilai max integer = " + Integer.MAX_VALUE);
+		// cara menentukan nilai max int
+		System.out.println("Nilai max byte = " + Byte.MAX_VALUE);
+		// cara menentukan nilai max byte
+		System.out.println("Nilai min integer = " + Integer.MIN_VALUE);
+		// cara menetukan nilai min int
+		System.out.println("Nilai min byte= " + Byte.MIN_VALUE);
+		// cara menentukan nilai min byte
+	}
+}
