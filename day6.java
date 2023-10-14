48ad6594794358ea29adb3cfa326a323954c1c62
milawatiD0223342 <147219691+milawatiD0223342@users.noreply.github.com>
2023-10-14 18:31:24 +0700

    Add files via upload

---
+++ day6.java
@@ -0,0 +1,12 @@
+package coding;
+public class Main {
+	public static void main(String[] args) {
+		short angka = 30; // short (satuan) 
+		System.out.println("SHORT");
+		System.out.println("nilai short angka = " + angka);
+		System.out.println("Nilai max = " + Short.MAX_VALUE);
+		System.out.println("Nilai min = " + Short.MIN_VALUE);
+		System.out.println("Besar short = " + Short.BYTES + " bytes");
+		System.out.println("Besar short = " + Short.SIZE + " bit");
+	}
+}
