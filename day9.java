d152d3079f39eabdd306bd148f586cedcbec1fc2
milawatiD0223342 <147219691+milawatiD0223342@users.noreply.github.com>
2023-10-17 21:01:03 +0700

    Add files via upload

---
+++ day9.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		// perkalian atau pembagian akan tetap terhitung duluan
+		int nilai = 30 + 20 * 5;
+		System.out.println(nilai);
+		nilai = 64 / 2 - 3 * 2;
+		System.out.println(nilai);
+		// pengelompokan operator aritmatika
+		// cara mengelompokkan dengan menggunakan tanda kurung 
+		nilai = 5 *(4 + 7);
+		System.out.println(nilai);
+	}
+}
