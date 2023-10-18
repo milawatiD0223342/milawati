e8cb77c25591ae5ec38016c444607fe5277f4fd9
milawatiD0223342 <147219691+milawatiD0223342@users.noreply.github.com>
2023-10-18 21:23:52 +0700

    Add files via upload

---
+++ day10.java
@@ -0,0 +1,29 @@
+public class Main {
+	public static void main(String[] args) {
+		byte a; // deklarasi karena tipe data byte a tidak memiliki nilai
+		a = 30;
+		System.out.println("nilai a = " + a);
+		short umur; // deklarasi
+		umur = 20;
+		System.out.println("nilai umur = " + umur);
+		int angka; // deklarasi
+		angka = 40;
+		System.out.println("nilai angka = " + angka);
+		long ukuran; // deklarasi
+		ukuran = 35;
+		System.out.println("nilai ukuran  = " + ukuran);
+		float jumlah; // deklarasi
+		jumlah = 30;
+		System.out.println("nilai jumlah = " + jumlah);
+		double tinggi; // deklarasi
+		tinggi = 5;
+		System.out.println("nilai tinggi = " + tinggi);
+	     char b; // deklarasi
+		b = '8';
+		System.out.println("nilai b = " + b);
+	    boolean status; // deklarasi
+		status = false;
+		System.out.println("nilai status = " +status);
+		
+	}
+}
