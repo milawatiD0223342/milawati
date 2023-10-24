8781d4113231271674a2811c9c9b028e8ae00a58
milawatiD0223342 <147219691+milawatiD0223342@users.noreply.github.com>
2023-10-24 17:57:17 +0700

    Add files via upload

---
+++ day16.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+	// operator percabangan
+	int data = 24;
+	System.out.println("data = " + data);
+	if (data == 23){ //false
+		System.out.println("bilangan genap");
+	} else if (data == 25){ // false
+	System.out.println("bilangan ganjil");
+		} else {
+		System.out.println("bilangan bulat");
+	}	
+	System.out.println("selesai");
+	}
+}
