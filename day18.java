b0d538ef426db8d86a9bea98699cca9ecd5f5982
milawatiD0223342 <147219691+milawatiD0223342@users.noreply.github.com>
2023-10-26 16:58:47 +0700

    Add files via upload

---
+++ day18.java
@@ -0,0 +1,32 @@
+public class Main {
+	public static void main(String[] args) {
+	// if bersarang
+			byte nilai = 25;
+		int nilai2 = 40;
+		
+		System.out.println("nilai pertama = 25 dan nilai kedua = 40");
+		
+		if (nilai >= 10){
+			if (nilai <= 40){
+				if (nilai == nilai2){
+					
+				System.out.println("ini adalah nilai positif");
+				
+			}else{	
+			
+				System.out.println("ini adalah nilai negatif");
+	}
+	
+		}else{
+			
+		System.out.println("ini adalah bilangan real");
+}
+
+		}else{
+		 
+   System.out.println("ini adalah bilangan bulat");
+	}
+	
+System.out.println("semuanya benar");
+}
+}
