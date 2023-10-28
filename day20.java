4b86b81ed60a224b3c2de2df2547800c89829bce
milawatiD0223342 <147219691+milawatiD0223342@users.noreply.github.com>
2023-10-28 12:05:27 +0700

    Add files via upload

---
+++ day20.java
@@ -0,0 +1,33 @@
+public class Main {
+	public static void main(String[] args) {
+		String alamat = "mamuju";
+		
+	// switch case
+	
+		switch (alamat) {
+			case "majene" : 
+			System.out.println("alamat saya di polman");
+			break;
+			
+			case "polman":
+			System.out.println("alamat saya di polman");
+			break;
+			
+			case "makassar" :
+			System.out.println("alamat saya di polman");
+			break;
+			
+			case "mamuju":
+			System.out.println("alamat saya di mamuju");
+			break;
+			
+			
+			default:
+			System.out.println("semuanya salah");
+			break;
+						
+			
+	}	
+	}
+}
+
