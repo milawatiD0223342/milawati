eae1cbff4777c401cb18e64644161dfe80d52928
milawatiD0223342 <147219691+milawatiD0223342@users.noreply.github.com>
2023-10-29 12:04:24 +0700

    Add files via upload

---
+++ day21.java
@@ -0,0 +1,21 @@
+public class Main {
+	public static void main(String[] args) {
+	
+		// operator assigment
+				int angka = 30;
+		angka += 32;
+		System.out.println("nilai angka = " +angka);
+		
+		int angka1= 30;
+		angka1 -= 25;
+		System.out.println("nilai angka1 = " +angka1);
+		
+		int angka2 = 30;
+		angka2 *= 30;
+		System.out.println("nilai angka2 = " +angka2);
+		
+		int angka3 = 120;
+		angka3 /= 60;
+		System.out.println("nilai angka3 = " +angka3);
+	}
+}
